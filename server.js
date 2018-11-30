7da2ea9ddecd1ec36b897c88fa13fc484d44421c
Luis Morneno <morenoluise535@gmail.com>
2018-11-29 21:06:45 -0800

    variables

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var 
+var reservation = []
+var waitlist = []
 
 
 app.listen(PORT, function() {
