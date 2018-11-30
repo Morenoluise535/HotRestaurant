3488bd6b1d0abd77d12315588d7e7f1b4dee89c6
Luis Morneno <morenoluise535@gmail.com>
2018-11-29 21:18:43 -0800

    thing

--- server.js
+++ server.js
@@ -15,6 +15,29 @@ app.use(express.json());
 var reservation = []
 var waitlist = []
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.get("/api/reservation", function(req, res) {
+    return res.json(reservation);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
+
+  
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
